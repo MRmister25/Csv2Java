0dcbd865e93a3ca5fa562d4f117e1a852ac6e05a
MRmister25 <revanthreddy26@gmail.com>
2018-10-25 13:25:27 +0530

    Final:parallel stream

--- src/Alt.java
+++ src/Alt.java
@@ -7,15 +7,15 @@ public class Alt {
 			Student stud=new Student();
 		List<Student> student = stud.reader("Test.csv");
 		//highest marks
-		student.stream().collect(Collectors.maxBy(Comparator.comparingInt(Student::getTotal))).ifPresent(System.out::println);
+		student.parallelStream().collect(Collectors.maxBy(Comparator.comparingInt(Student::getTotal))).ifPresent(System.out::println);
 		// lowest marks
-		student.stream().collect(Collectors.minBy(Comparator.comparingInt(Student::getTotal))).ifPresent(System.out::println);
+		student.parallelStream().collect(Collectors.minBy(Comparator.comparingInt(Student::getTotal))).ifPresent(System.out::println);
 		// average marks in the school
-		Double avg=student.stream().collect(Collectors.averagingInt(Student::getTotal));
+		Double avg=student.parallelStream().collect(Collectors.averagingInt(Student::getTotal));
 		System.out.println(avg);
 		//dept wise highest marks
 		Map<String, Optional<Student>> dept= 
-				student.stream().collect(Collectors.groupingBy(Student::getDept,
+				student.parallelStream().collect(Collectors.groupingBy(Student::getDept,
 						Collectors.maxBy(Comparator.comparingInt(Student::getTotal))
 						
 						)
